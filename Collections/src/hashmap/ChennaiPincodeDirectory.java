5d2081b91766509f464aafec026419e4bd78909c
Venkat <Venkat.bca0@gmail.com>
2020-09-26 16:05:30 +0530

    TreeMap
    Chennai PinCode Directory

---
+++ Collections/src/hashmap/ChennaiPincodeDirectory.java
@@ -0,0 +1,30 @@
+package hashmap;
+
+import java.util.*;
+
+import hashmap.backend.DirectoryBackend;
+
+/*
+ * Chennai Pincode Directory
+ * Find any Pincode and Areas in chennai
+ * Runner class to get input from the user to find the Pin code or area
+ */
+
+public class ChennaiPincodeDirectory {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println(" Welcome to Chennai City Pincode Directory !" + "\n\n"
+				+ "Enter the area code or area name to begin search" + "\n");
+
+		String userInput = scan.next().strip();
+
+		DirectoryBackend.findInDirectory(userInput);
+
+		scan.close();
+
+	}
+
+}
